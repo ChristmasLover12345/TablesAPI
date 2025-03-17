2feffa12e573d4700294f0d1cc967d3f0a0172e2
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-17 15:56:13 -0700

    started on the tables

---
+++ Models/UserProfileModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TablesAPI.Models
+{
+    public class UserProfileModel
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? BikeType { get; set; }
+        public string? RidingExperience { get; set; }
+        public string? RidingPreference { get; set; }
+        public string? RideConsistency  { get; set; }
+        public string? ProfilePicture { get; set; }
+    }
+}
