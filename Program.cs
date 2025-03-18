5e974c25d4d0b6f59f1b41e1c997eae60713a830
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-18 12:42:16 -0700

    dtuff

--- Program.cs
+++ Program.cs
@@ -13,6 +13,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>{
+    options.AddPolicy("AllowAll",
+        policy=>{
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
 
@@ -26,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
--- Services/RideTablesService.cs
+++ Services/RideTablesService.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TablesAPI.Context;
 
 namespace TablesAPI.Services
 {
     public class RideTablesService
     {
         
+         private readonly DataContext _dataContext;
+        public RideTablesService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
     }
 }
